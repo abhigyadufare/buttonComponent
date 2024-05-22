8b7ae4c5c4d147e4a7e6154f8ebd375246f2ca24
Abhigya <abhigyadufare@gmail.com>
2024-05-22 17:21:57 +0530

    pathname

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const Button = () => {
     const [hostname, setHostname] = useState('');
     const handleSubmit = () => {
-        setHostname(window.location.origin);
+        setHostname(document.location.pathname);
       };
   return (
     <>
