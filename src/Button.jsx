66fc42565a4c3c30d54540cf6974e42bf3d5615a
Abhigya <abhigyadufare@gmail.com>
2024-05-22 17:19:34 +0530

    added origin

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const Button = () => {
     const [hostname, setHostname] = useState('');
     const handleSubmit = () => {
-        setHostname(window.location.href);
+        setHostname(window.location.origin);
       };
   return (
     <>
